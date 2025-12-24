8e0518aef68b0c8964f2b64d660c058efd762856
Hans <Hans@fanruan.com>
2025-12-24 13:41:28 +0800

    layout & web container instance

---
+++ entry/src/main/resources/rawfile/js/mainPage.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('sendBtn');
+  btn.addEventListener('click', () => {
+    PostMsgToEts("PostMsgToEts");
+  });
+});
+
+var h5Port;
+var output = document.querySelector('.output');
+window.addEventListener('message', function (event) {
+  showPopup(0);
+  if (event.data == '__init_port__') {
+    showPopup(1);
+    if (event.ports[0] != null) {
+      h5Port = event.ports[0];
+      showPopup(event.ports[0]);// 1. 保存从ets侧发送过来的端口
+      h5Port.onmessage = function (event) {
+        // 2. 接收ets侧发送过来的消息.
+        var msg = 'Got message from ets:';
+        var result = event.data;
+        if (typeof(result) == "string") {
+          console.info("received string message from html5, string is:" + result);
+          msg = msg + result;
+        } else if (typeof(result) == "object") {
+          if (result instanceof ArrayBuffer) {
+            console.info("received arraybuffer from html5, length is:" + result.byteLength);
+            msg = msg + "length is " + result.byteLength;
+          } else {
+            console.info("not support");
+          }
+        } else {
+          console.info("not support");
+        }
+        output.innerHTML = msg;
+      }
+    }
+  }
+})
+
+// 3. 使用h5Port往ets侧发送消息.
+function PostMsgToEts(data) {
+  if (h5Port) {
+    h5Port.postMessage(data);
+  } else {
+    console.error("h5Port is null, Please initialize first");
+  }
+}
+
+
+
+function showPopup(msg) {
+  const popup = document.getElementById('popup');
+  document.getElementById('popupMsg').innerText = msg;
+  popup.style.display = 'block';
+}
+
+document.getElementById('popupClose').addEventListener('click', () => {
+  document.getElementById('popup').style.display = 'none';
+});
